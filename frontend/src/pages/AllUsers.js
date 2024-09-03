5a55e31a9d586251c5ebdb7ff90ecd1f9031f192
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-09-03 16:28:36 +0500

    update Logout

--- frontend/src/pages/AllUsers.js
+++ frontend/src/pages/AllUsers.js
@@ -43,7 +43,7 @@ const AllUsers = () => {
     <div className='bg-white pb-4'>
       <table className='w-full userTable'>
         <thead>
-          <tr>
+          <tr  className='bg-black text-white'>
             <th>Sr.</th>
             <th>Name</th>
             <th>Email</th>
@@ -56,7 +56,7 @@ const AllUsers = () => {
           {
             allUser.map((el,index) => {
               return(
-                <tr className='bg-black text-white'>
+                <tr>
                   <td>{index+1}</td>
                   <td>{el?.name}</td>
                   <td>{el?.email}</td>
